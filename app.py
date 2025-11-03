f7a14f4b89016363742fa0524acad0a30e9076ab
Rangsiya12 <rangsiyawongpan9@gmail.com>
2025-11-03 16:04:32 +0700

    Change MODEL_PATH to 'models'
    Update model path to point to the models directory

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 # โหลด YOLO model
-MODEL_PATH = 'models/best.pt'
+MODEL_PATH = 'models'
 model = None
 
 if ULTRALYTICS_AVAILABLE and TORCH_AVAILABLE and NUMPY_AVAILABLE:
